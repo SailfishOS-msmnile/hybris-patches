dcdd7d18510185cab1624ec7f0b34eeee9839ad3
dopaemon <polarisdp@gmail.com>
2022-08-15 06:15:08 +0000

    Patches: added Patches Xiaomi Sweet
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/framesequence/samples/FrameSequenceSamples/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/common/tests/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/framesequence/jni/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/calendar/tests/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/camera2/portability/tests/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/camera2/utils/tests/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/common/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/framesequence/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/calendar/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/camera2/portability/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/camera2/public/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/ex/camera2/utils/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/chips/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/chips/tests/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/chips/sample/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/timezonepicker/Android.bp
+rm $ANDROID_ROOT/packages/apps/Etar/external/colorpicker/Android.bp
+
 set -e
 
 MB=$1
